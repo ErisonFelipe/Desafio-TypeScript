ab0add9940069857391b862f0ce4aa788e7051ef
Erison Felipe <erison274@gmail.com>
2022-07-11 21:48:27 -0300

    first commit

---
+++ desafio1.ts
@@ -0,0 +1,11 @@
+// Descubra como executar sem erros.
+
+interface Funcionarios {
+    code: number,
+    name: string,
+}
+
+let employee: Funcionarios = {
+    code: 10,
+    name: "João",
+};
---
+++ desafio2.ts
@@ -0,0 +1,32 @@
+// Como podemos melhorar o esse código usando TS? 
+
+interface Humano {
+    nome: string,
+    idade: number,
+    profissao: string
+};
+
+let pessoa1: Humano = {
+    nome: "maria",
+    idade: 29,
+    profissao: "atriz"
+};
+
+let pessoa2: Humano = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+};
+
+let pessoa3: Humano = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+
+let pessoa4: Humano = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
+
